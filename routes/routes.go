546ef33bf44f2ba512812ddb011d322054514e49
Ilnur Saifutdinov <ilnursoft@gmail.com>
2016-02-17 11:47:32 +0300

    add new func GetPostsNewPos

--- routes/routes.go
+++ routes/routes.go
@@ -2,7 +2,7 @@
 package routes
 
 import (
-	"fmt"
+	//	"fmt"
 	"net/http"
 	"os"
 	"strconv"
@@ -24,8 +24,8 @@ var (
 	Tekpost      int    // номер сообщения с которого начинается сообщения на странице
 )
 
-// получить сообщения из папки pathposts в кол-ве kolpost
-func GetPostsNew(pathposts string, kolpost int) ([]models.Post, int) {
+// получить сообщения из папки pathposts в кол-ве kolpost начиная с позиции tekpos
+func GetPostsNewPos(pathposts string, tekpos int, kolpost int) ([]models.Post, int) {
 	var (
 		pp       models.Post
 		kolfiles int
@@ -37,9 +37,15 @@ func GetPostsNew(pathposts string, kolpost int) ([]models.Post, int) {
 	namefs = utils.SorttoDown(namefs)
 	namefs = utils.ConcatPathFileName(namefs, pathposts+string(os.PathSeparator))
 	kolfiles = len(namefs)
+	if tekpos < 0 {
+		tekpos = 0
+	}
+	if !(tekpos < kolfiles) {
+		tekpos = kolfiles - 1
+	}
 
 	tekkolpost := 0 // кол-во постов
-	tekpos := 0
+	//	tekpos := 0
 	for (tekpos != -1) && (tekpos < kolfiles) && (tekkolpost < kolpost) {
 		tekpos = pp.GetNormalPost(namefs, tekpos)
 		if tekpos != -1 {
@@ -52,68 +58,10 @@ func GetPostsNew(pathposts string, kolpost int) ([]models.Post, int) {
 	return p, kolfiles
 }
 
-// получить сообщения из папки pathposts в кол-ве kolpost
-func getPosts(pathposts string, kolpost int) ([]models.Post, int) {
-	var (
-		//		tnamefs []string
-		//		pp       models.Post
-		kolfiles int
-	)
-
-	p := make([]models.Post, 0)
-
-	namefs := utils.Getlistfileindirectory(pathposts)
-	namefs = utils.SorttoDown(namefs)
-	kolfiles = len(namefs)
-
-	//	if (kolfiles != 0) && (kolpost != 0) {
-
-	//		if kolpost > len(namefs) {
-	//			tnamefs = namefs[:]
-	//			for npos, _ := range tnamefs {
-	//				if pp.GetNormalPost(tnamefs, npos) {
-	//					p = append(p, pp)
-	//				}
-	//			}
-
-	//		} else {
-	//			tnamefs = namefs[:kolpost]
-	//		}
-
-	//		for _, namef := range tnamefs {
-	//			pp.GetPostfromFileMd(Pathposts + string(os.PathSeparator) + namef)
-	//			if !pp.GetDraft() { // не отражаются черновики
-	//				p = append(p, pp)
-	//			} else {
-	//				kolpost += 1
-	//				if kolpost > len(namefs) {
-	//				}
-	//			}
-
-	//		}
-	//	} else {
-	//		pp.New()
-	//		pp.Title = "ЭТОТ БЛОГ ПУСТ. ПРИХОДИТЕ ПОЗЖЕ ;)"
-	//		p = append(p, pp)
-	//	}
-	return p, kolfiles
-}
-
 //-----------END вспомогательная функция которую надо будет удалить со временем
 
 func IndexHandler(rr render.Render, w http.ResponseWriter, r *http.Request) {
-	//	var pp models.Post
-	//	p := make([]models.Post, 0)
-	fmt.Println(Pathposts)
-	p, vsegopost := GetPostsNew(Pathposts, Kolpost)
-	fmt.Println(len(p))
-	for _, v := range p {
-		fmt.Print(v)
-		//		fmt.Print(v.Id)
-		//		fmt.Print("  ", v.Draft)
-		//		fmt.Println("  ", v.SmallContentText)
-
-	}
+	p, vsegopost := GetPostsNewPos(Pathposts, 0, Kolpost)
 	rr.HTML(200, "index", &models.PagePost{TitlePage: "Блог проектов kaefik", Posts: p, Postright: vsegopost - Kolpost})
 }
 
@@ -137,56 +85,79 @@ func HtmlHandler(rr render.Render, w http.ResponseWriter, r *http.Request, param
 
 // просмотр посты блога
 func ViewHandler(rr render.Render, w http.ResponseWriter, r *http.Request, params martini.Params) {
-	var pp models.Post
-	p := make([]models.Post, 0)
+	//	var pp models.Post
+	//	p := make([]models.Post, 0)
 	numpost, _ := strconv.Atoi(params["numpost"])
-	namefs := utils.Getlistfileindirectory(Pathposts)
-	namefs = utils.SorttoUp(namefs)
-	tnamefs := namefs
-	vsegopost := len(namefs)
 
-	if numpost <= 0 {
-		numpost = len(namefs)
-	}
+	p, kolfiles := GetPostsNewPos(Pathposts, numpost, Kolpost)
 
-	if numpost >= len(namefs) {
-		if Kolpost > len(namefs) {
-			tnamefs = namefs[:]
-		} else {
-			tnamefs = namefs[len(namefs)-Kolpost:]
-		}
-
-		if len(namefs) != 0 {
-			for k := len(tnamefs) - 1; k >= 0; k-- {
-				namef := tnamefs[k]
-				pp.GetPostfromFileMd(Pathposts + string(os.PathSeparator) + namef)
-				p = append(p, pp)
-			}
-		} else {
-			p = append(p, models.Post{Id: "ПОСТОВ НЕТ", Title: "ЭТОТ БЛОГ ПУСТ. ПРИХОДИТЕ ПОЗЖЕ ;)", ContentText: ""})
-		}
-		rr.HTML(200, "view", &models.PagePost{TitlePage: "Блог проектов kaefik", Posts: p, Postright: vsegopost - Kolpost})
-		return
-	}
-	if len(namefs) != 0 {
-		kk := numpost - Kolpost
-		if kk <= 0 {
-			kk = 0
-		} else {
-			kk = numpost - Kolpost // - 1
-		}
-		tnamefs := namefs[kk:numpost]
-
-		if len(namefs) != 0 {
-			for k := len(tnamefs) - 1; k >= 0; k-- {
-				namef := tnamefs[k]
-				pp.GetPostfromFileMd(Pathposts + string(os.PathSeparator) + namef)
-				p = append(p, pp)
-			}
-		}
-	} else {
+	if kolfiles == 0 {
 		rr.Redirect("/")
 		return
+
 	}
+
+	//		if len(namefs) != 0 {
+	//			for k := len(tnamefs) - 1; k >= 0; k-- {
+	//				namef := tnamefs[k]
+	//				pp.GetPostfromFileMd(Pathposts + string(os.PathSeparator) + namef)
+	//				p = append(p, pp)
+	//			}
+	//		}
+	//	} else {
+	//		rr.Redirect("/")
+	//		return
+	//	}
+
+	//----
+	//	namefs := utils.Getlistfileindirectory(Pathposts)
+	//	namefs = utils.SorttoUp(namefs)
+	//	tnamefs := namefs
+	//	vsegopost := len(namefs)
+
+	//	if numpost <= 0 {
+	//		numpost = len(namefs)
+	//	}
+
+	//	if numpost >= len(namefs) {
+	//		if Kolpost > len(namefs) {
+	//			tnamefs = namefs[:]
+	//		} else {
+	//			tnamefs = namefs[len(namefs)-Kolpost:]
+	//		}
+
+	//		if len(namefs) != 0 {
+	//			for k := len(tnamefs) - 1; k >= 0; k-- {
+	//				namef := tnamefs[k]
+	//				pp.GetPostfromFileMd(Pathposts + string(os.PathSeparator) + namef)
+	//				p = append(p, pp)
+	//			}
+	//		} else {
+	//			p = append(p, models.Post{Id: "ПОСТОВ НЕТ", Title: "ЭТОТ БЛОГ ПУСТ. ПРИХОДИТЕ ПОЗЖЕ ;)", ContentText: ""})
+	//		}
+	//		rr.HTML(200, "view", &models.PagePost{TitlePage: "Блог проектов kaefik", Posts: p, Postright: vsegopost - Kolpost})
+	//		return
+	//	}
+	//	if len(namefs) != 0 {
+	//		kk := numpost - Kolpost
+	//		if kk <= 0 {
+	//			kk = 0
+	//		} else {
+	//			kk = numpost - Kolpost // - 1
+	//		}
+	//		tnamefs := namefs[kk:numpost]
+
+	//		if len(namefs) != 0 {
+	//			for k := len(tnamefs) - 1; k >= 0; k-- {
+	//				namef := tnamefs[k]
+	//				pp.GetPostfromFileMd(Pathposts + string(os.PathSeparator) + namef)
+	//				p = append(p, pp)
+	//			}
+	//		}
+	//	} else {
+	//		rr.Redirect("/")
+	//		return
+	//	}
+	//---
 	rr.HTML(200, "view", &models.PagePost{TitlePage: "Блог проектов kaefik", Posts: p, Postleft: numpost + Kolpost, Postright: numpost - Kolpost})
 }
--- routes/routes_test.go
+++ routes/routes_test.go
@@ -12,13 +12,36 @@ import (
 // получить сообщения из папки pathposts в кол-ве kolpost
 //func GetPostsNew(pathposts string, kolpost int) ([]models.Post, int) {
 
-func TestGetPostsNew(t *testing.T) {
+//func TestGetPostsNew(t *testing.T) {
+//	var (
+//		pathposts string = "test-postmd"
+//		kolpost   int    = 3
+//	)
+
+//	resposts, kolfiles := GetPostsNew(pathposts, kolpost)
+
+//	if kolfiles != 8 {
+//		t.Fatalf("должен быть kolfiles == 8  - ", kolfiles)
+//	}
+//	if (resposts[0].Id != pathposts+string(os.PathSeparator)+"6.md") && (resposts[1].Id != pathposts+string(os.PathSeparator)+"5.md") && (resposts[2].Id != pathposts+string(os.PathSeparator)+"4.md") {
+//		t.Fatalf("должен быть 6.md , 5.md, 4.md  - ", resposts[0].Id+" "+resposts[1].Id+" "+resposts[2].Id+" ")
+//	}
+
+//	if (resposts[0].SmallContentText == "") && (resposts[1].SmallContentText == "") && (resposts[2].SmallContentText == "") {
+//		t.Fatalf("SmallContentText должен быть не пустой - ", resposts[0].SmallContentText+" -- "+resposts[1].SmallContentText+" -- "+resposts[2].SmallContentText+" -- ")
+//	}
+
+//}
+
+// получить сообщения из папки pathposts в кол-ве kolpost начиная с позиции tekpos
+//func GetPostsNewPos(pathposts string, tekpos int, kolpost int) ([]models.Post, int) {
+func TestGetPostsNewPos(t *testing.T) {
 	var (
 		pathposts string = "test-postmd"
 		kolpost   int    = 3
 	)
 
-	resposts, kolfiles := GetPostsNew(pathposts, kolpost)
+	resposts, kolfiles := GetPostsNewPos(pathposts, 0, kolpost)
 
 	if kolfiles != 8 {
 		t.Fatalf("должен быть kolfiles == 8  - ", kolfiles)
@@ -30,5 +53,17 @@ func TestGetPostsNew(t *testing.T) {
 	if (resposts[0].SmallContentText == "") && (resposts[1].SmallContentText == "") && (resposts[2].SmallContentText == "") {
 		t.Fatalf("SmallContentText должен быть не пустой - ", resposts[0].SmallContentText+" -- "+resposts[1].SmallContentText+" -- "+resposts[2].SmallContentText+" -- ")
 	}
+	//----
+	resposts, kolfiles = GetPostsNewPos(pathposts, -1, kolpost)
 
+	if kolfiles != 8 {
+		t.Fatalf("должен быть kolfiles == 8  - ", kolfiles)
+	}
+	if (resposts[0].Id != pathposts+string(os.PathSeparator)+"6.md") && (resposts[1].Id != pathposts+string(os.PathSeparator)+"5.md") && (resposts[2].Id != pathposts+string(os.PathSeparator)+"4.md") {
+		t.Fatalf("должен быть 6.md , 5.md, 4.md  - ", resposts[0].Id+" "+resposts[1].Id+" "+resposts[2].Id+" ")
+	}
+
+	if (resposts[0].SmallContentText == "") && (resposts[1].SmallContentText == "") && (resposts[2].SmallContentText == "") {
+		t.Fatalf("SmallContentText должен быть не пустой - ", resposts[0].SmallContentText+" -- "+resposts[1].SmallContentText+" -- "+resposts[2].SmallContentText+" -- ")
+	}
 }
